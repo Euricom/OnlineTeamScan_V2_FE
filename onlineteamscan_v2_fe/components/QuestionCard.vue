5f05f9b52e68df4e14ac4671e72fdf9c5bdda0f6
VincentHertens <55692053+VincentHertens@users.noreply.github.com>
2021-04-08 15:41:15 +0200

    Apply feedback

--- onlineteamscan_v2_fe/components/QuestionCard.vue
+++ onlineteamscan_v2_fe/components/QuestionCard.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <v-card style="margin: 5px 15px 15px;" elevation="1">
-      <v-card-title style="font-weight: bold; font-size: 18px; color: #343A40;">Vraag {{ question.question.number }}</v-card-title>
-      <v-card-text style="font-size: 16px; color: #212121; padding-bottom: 0">
+    <v-card class="question-card" elevation="1">
+      <v-card-title class="question-card-title">Vraag {{ question.question.number }}</v-card-title>
+      <v-card-text class="question-card-text">
         {{ question.text }}
       </v-card-text>
-      <v-card-actions style="padding-bottom: 0; padding-top: 0">
-        <v-radio-group row mandatory v-model="answer">
+      <v-card-actions class="question-card-actions">
+        <v-radio-group row mandatory v-model="score">
           <v-radio label="1" value="1"/>
           <v-radio label="2" value="2"/>
           <v-radio label="3" value="3"/>
@@ -21,8 +21,6 @@
 <script>
 export default {
   name: 'QuestionCard',
-  components: {
-  },
   props: {
     question: {
       type: Object,
@@ -31,13 +29,33 @@ export default {
   },
   data() {
     return {
-      answer: null,
+      score: null,
     }
   },
   methods: {
       returnAnswer() {
-        return {"DysfunctionId": this.question.question.dysfunctionId, "Score": this.answer}
+        return {"DysfunctionId": this.question.question.dysfunctionId, "Score": this.score}
       },
     }
 }
 </script>
+
+<style scoped>
+.question-card-title {
+  font-weight: bold;
+  font-size: 18px;
+  color: #343A40;
+}
+.question-card-text {
+  font-size: 16px;
+  color: #212121;
+  padding-bottom: 0;
+}
+.question-card {
+ margin: 5px 15px 15px;
+}
+.question-card-actions {
+  padding-bottom: 0;
+  padding-top: 0;
+}
+</style>
--- onlineteamscan_v2_fe/pages/teamscan/_teamscan.vue
+++ onlineteamscan_v2_fe/pages/teamscan/_teamscan.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
     <v-toolbar elevation="0" extended>
-      <v-toolbar-title
-        class="font-weight-medium toolbar-title">
+      <v-toolbar-title class="font-weight-medium toolbar-title">
         Teamscan 8
       </v-toolbar-title>
       <template v-slot:extension>
-        <div style="margin-bottom: 25px;">
+        <div class="sub-toolbar-title-position">
           <h1 class="font-weight-medium sub-toolbar-title">Gestart door: Thomas Claessens</h1>
           <h1 class="font-weight-medium sub-toolbar-title">Startdatum: 15/11/2020</h1>
         </div>
@@ -14,16 +13,15 @@
     </v-toolbar>
 
     <v-container fluid>
-      <QuestionCard ref="answers" v-for="question in questions" :key="question.question.id" :question="question"/>
+      <QuestionCard ref="questionAnswers" v-for="question in questions" :key="question.question.id" :question="question"/>
 
       <v-btn class="final-button" color="#71BF42">
-        <span style="color: white; font-weight: bold;" @click="save">Opslaan</span>
+        <span class="final-button-save-button" @click="saveTeamscan">Opslaan</span>
       </v-btn>
       <v-btn class="final-button" color="#FFFFFF">
-        <span style="color: #212121; font-weight: bold;" @click="cancel">Annuleren</span>
+        <span class="final-button-cancel-button" @click="cancel">Annuleren</span>
       </v-btn>
     </v-container>
-
   </div>
 </template>
 
@@ -40,22 +38,21 @@ export default {
       answers: [],
     }
   },
-  created() {
-    this.$axios.get(`questiontranslations/language/${this.$auth.user.preferredLanguageId}`).then(res => this.questions = res.data).catch(err => console.log(err))
+  async created() {
+    let questions = await this.$axios.get(`questiontranslations/language/${this.$auth.user.preferredLanguageId}`)
+    this.questions = questions.data
   },
   methods: {
     cancel() {
 
     },
-    save() {
-      let i;
-      for (i = 0; i < this.questions.length; i++) {
-        this.answers.push(this.$refs.answers[i].returnAnswer())
-      }
+    saveTeamscan() {
+      this.answers = this.$refs.questionAnswers.map(x => x.returnAnswer())
+      console.log(this.answers)
 
-      this.$axios.post(`individualscores/3/1`, this.answers)
+      /*this.$axios.post(`individualscores/3/1`, this.answers)
         .then(res => console.log(res.data))
-        .catch(err => console.log(err))
+        .catch(err => console.log(err))*/
     },
   },
 }
@@ -70,6 +67,9 @@ export default {
   font-size: 16px;
   color: #A8A8A8
 }
+.sub-toolbar-title-position {
+  margin-bottom: 25px;
+}
 .final-button {
   float: right;
   font-weight: normal;
@@ -78,5 +78,13 @@ export default {
   margin-right: 15px;
   margin-bottom: 15px;
   width: 110px;
+}
+.final-button-save-button {
+  color: white;
+  font-weight: bold;
+}
+.final-button-cancel-button {
+  color: #212121;
+  font-weight: bold;
 }
 </style>
