ba423f7c2d03d4a572e9371ffe4975aa961c9ee8
H2KKI2 <yanu.szapinszky@gmail.com>
2021-05-03 16:50:17 +0200

    Update nuxt.config.js

--- onlineteamscan_v2_fe/nuxt.config.js
+++ onlineteamscan_v2_fe/nuxt.config.js
@@ -1,7 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 import {defaults} from "@nuxtjs/vuetify/dist/options";
 
-module.exports = {
+export default {
   target: 'static',
   ssr: false,
   head: {
