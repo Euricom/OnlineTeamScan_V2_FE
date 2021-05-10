0d9788e625a2de20ed30e36bbf94fe0d6678b334
VincentHertens <55692053+VincentHertens@users.noreply.github.com>
2021-05-10 18:45:55 +0200

    Add pdf layout

--- onlineteamscan_v2_fe/components/ScoreCard.vue
+++ onlineteamscan_v2_fe/components/ScoreCard.vue
@@ -128,14 +128,6 @@ export default {
       };
     },
   },
-  methods: {
-/*    calculateLevel(score) {
-      if (score >= this.levels[0].lowerLimit && score <= this.levels[0].upperLimit) return this.levels[0]
-      if (score >= this.levels[1].lowerLimit && score <= this.levels[1].upperLimit) return this.levels[1]
-      if (score >= this.levels[2].lowerLimit && score <= this.levels[2].upperLimit) return this.levels[2]
-      return this.levels[3]
-    },*/
-  },
 }
 </script>
 
